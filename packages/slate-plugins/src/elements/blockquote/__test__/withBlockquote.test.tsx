a3f8186b8960e0338b39957443aa1ff0d9f4d509
GuilhemBrd <beillard.guilhem@gmail.com>
2021-02-24 20:23:29 +0100

    clean

--- packages/slate-plugins/src/elements/blockquote/__test__/withBlockquote.test.tsx
+++ packages/slate-plugins/src/elements/blockquote/__test__/withBlockquote.test.tsx
@@ -2,16 +2,16 @@
 
 import { Editor } from 'slate';
 import { withReact } from 'slate-react';
-import { jsx } from '../../__test-utils__/jsx';
-import { withInlineVoid } from '../../common/plugins/inline-void/withInlineVoid';
-import { ELEMENT_LINK } from '../link/defaults';
-import { withBlockquote } from './index';
+import { jsx } from '../../../__test-utils__/jsx';
+import { withInlineVoid } from '../../../common/plugins/inline-void/withInlineVoid';
+import { ELEMENT_LINK } from '../../link/defaults';
+import { withBlockquote } from '../index';
 
 describe('normalizeBlockquote', () => {
   it('should insert a p', () => {
     const input = ((
       <editor>
-        <hblockquote>tes<cursor /></hblockquote>
+        <blockquote>tes<cursor /></blockquote>
       </editor>
     ) as any) as Editor;
 
