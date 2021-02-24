759f6ef2f706c420703a070bd892ad210aaff164
GuilhemBrd <beillard.guilhem@gmail.com>
2021-02-24 19:51:36 +0100

    clean

--- packages/slate-plugins/src/elements/blockquote/withBlockquote.ts
+++ packages/slate-plugins/src/elements/blockquote/withBlockquote.ts
@@ -1,29 +1,14 @@
 import { ReactEditor } from 'slate-react';
-import { Node, Path, Transforms } from 'slate';
-import { getAbove, insertEmptyElement, setDefaults } from '../../common';
+import { Node, NodeEntry, Path } from 'slate';
+import {  insertEmptyElement, setDefaults } from '../../common';
 import { DEFAULTS_BLOCKQUOTE } from './defaults';
-import { someNode } from '../../common/queries/someNode';
 import { DEFAULTS_PARAGRAPH } from '../paragraph';
-import { match } from '../../../common/utils/match';
 
 export const withBlockquote = () => <T extends ReactEditor>(editor: T) => {
-  const { insertData, insertText, normalizeNode } = editor;
+  const { normalizeNode } = editor;
   const { blockquote, p } = setDefaults({}, { ...DEFAULTS_BLOCKQUOTE, ...DEFAULTS_PARAGRAPH });
 
-  editor.insertData = (data: DataTransfer) => {
-    const text = data.getData('text/plain');
-    if (text) {
-      if (someNode(editor, { match: { type: blockquote.type } })) {
-        const current = getAbove(editor);
-        insertEmptyElement(editor, p.type, { at: current?.[1] });
-        return insertData(data);
-      }
-    }
-
-    insertData(data);
-  };
-
-  editor.normalizeNode = (entry) => {
+  editor.normalizeNode = (entry: NodeEntry<Node>) => {
     const [node, path] = entry;
     if (node.type === blockquote.type) {
       const firstChild: Node = (node.children as Node[])?.[0];
